c7d7602e5fb04e5b431b35e8a5025354167032be
mark <mark.korput@gmail.com>
2018-09-02 16:02:38 +0200

    fix for more recent develop-of0.9 branch of ofxCubeMap

--- src/ofxEquiMap.cpp
+++ src/ofxEquiMap.cpp
@@ -7,22 +7,22 @@
 namespace ofxEquiMap {
     static string warp_frag_shader_str = STRINGIFY(
        uniform samplerCube envMap;
-       
+
        void main() {
-           
+
            vec2 tc = gl_TexCoord[0].st / vec2(2.0) + 0.5;  //only line modified from the shader toy example
            vec2 thetaphi = ((tc * 2.0) - vec2(1.0)) * vec2(3.1415926535897932384626433832795, 1.5707963267948966192313216916398);
            vec3 rayDirection = vec3(cos(thetaphi.y) * cos(thetaphi.x), sin(thetaphi.y), cos(thetaphi.y) * sin(thetaphi.x));
-           
+
            gl_FragColor = textureCube(envMap, rayDirection);
        }
        );
-    
+
     void Renderer::setup(int size, Scene* s, int internalformat)
     {
         int type = ofGetGlTypeFromInternal(internalformat);
         int format = ofGetGLFormatFromInternal(internalformat);
-        cm.initEmptyTextures(size, format, type);
+        cm.init(size, format, type);
         warpShader.setupShaderFromSource(GL_FRAGMENT_SHADER, warp_frag_shader_str);
         warpShader.linkProgram();
         registerScene(s);
@@ -41,20 +41,20 @@ namespace ofxEquiMap {
             cm.endDrawingInto3D();
         }
     }
-    
+
     void Renderer::draw(float x, float y, float w, float h) {
         warpShader.begin();
         warpShader.setUniform1i("envMap", 0);
         cm.drawFace(GL_TEXTURE_CUBE_MAP_POSITIVE_Z, x, y, w, h);
         warpShader.end();
     }
-    
+
     void CustomFboRenderer::setup(int size, Scene* s, int internalformat, int numSamples)
     {
         Renderer::setup(size, s, internalformat);
         fbo.allocate(cm.getWidth(), cm.getHeight(), internalformat, numSamples);
     }
-    
+
     void CustomFboRenderer::setup(int size, Scene* s, ofFbo::Settings fbo_settings)
     {
         Renderer::setup(size, s, fbo_settings.internalformat);
@@ -66,23 +66,23 @@ namespace ofxEquiMap {
             fbo.begin();
             ofClear(0);
             ofPushView();
-            
+
             glMatrixMode( GL_PROJECTION );
             glLoadIdentity();
             glLoadMatrixf( cm.getProjectionMatrix().getPtr() );
-            
+
             glMatrixMode( GL_MODELVIEW );
             glLoadIdentity();
             glLoadMatrixf( cm.getLookAtMatrixForFace( GL_TEXTURE_CUBE_MAP_POSITIVE_X + i).getPtr() );
-            
+
             // work around for ofLight issue caused by ofxCubeMap
             ofLoadViewMatrix(ofGetCurrentMatrix(OF_MATRIX_MODELVIEW));
-            
+
             scene->drawEquiScene();
-            
+
             ofPopView();
             fbo.end();
-            
+
             cm.beginDrawingInto2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i);
             ofClear(0);
             fbo.draw(0, 0);
