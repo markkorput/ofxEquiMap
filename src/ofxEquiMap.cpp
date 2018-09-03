1f2d4e6ff458e18cc6ab1424e824cb0a0fd60b68
mark <mark.korput@gmail.com>
2018-09-03 15:02:17 +0200

    add support for programmable renderer pipeline with new shader and render (draw) method

--- src/ofxEquiMap.cpp
+++ src/ofxEquiMap.cpp
@@ -6,6 +6,8 @@
 
 namespace ofxEquiMap {
 
+    // shaders
+
     static string warp_frag_shader_str = STRINGIFY(
         uniform samplerCube envMap;
 
@@ -18,46 +20,45 @@ namespace ofxEquiMap {
     );
 
     string warp_vert_shader_str330 = string("#version 330\nprecision highp float;\n") + STRINGIFY(
-        //
-        layout(location = 0) in vec4  position;
-        layout(location = 1) in vec4  color;
-        layout(location = 2) in vec3  normal;
-        layout(location = 3) in vec2  texcoord;
 
-        uniform mat4 projectionMatrix;
-        uniform mat4 modelViewMatrix;
+        layout(location = 0) in vec3  position;
+        // layout(location = 1) in vec4  color;
+        // layout(location = 2) in vec3  normal;
+        layout(location = 1) in vec2  texcoord;
+
+        // // uniform mat4 projectionMatrix;
+        // // uniform mat4 modelViewMatrix;
         uniform mat4 modelViewProjectionMatrix;
-        uniform mat4 normalMatrix;
+        // // uniform mat4 normalMatrix;
+        uniform vec2 dimmensions;
 
-        out VertexAttrib {
-        vec3 texcoord;
-        } vertex;
+        out vec3 verttexcoord;
 
-        void main(void)
-        {
-            vertex.texcoord = normalize(position.xyz);
-            gl_Position = modelViewProjectionMatrix * position;
+        void main() {
+            gl_Position = modelViewProjectionMatrix * vec4(position, 1);
+            // vertex.texcoord = texcoord;
+            verttexcoord  = vec3(position.xy/dimmensions, 0); // normalize(position);
         }
     );
 
     string warp_frag_shader_str330 = string("#version 330\nprecision highp float;\n") + STRINGIFY(
 
         uniform samplerCube envMap;
-
-        in VertexAttrib {
-            vec3 texcoord;
-        } vertex;
-
+        in vec3 verttexcoord;
         out vec4 fragColor;
 
         void main (void)
         {
-            vec2 tc = vertex.texcoord.st / vec2(2.0) + 0.5;  //only line modified from the shader toy example
+            // fragColor = vec4(1,0,0,1);
+
+            vec2 tc = verttexcoord.st;  //only line modified from the shader toy example
             vec2 thetaphi = ((tc * 2.0) - vec2(1.0)) * vec2(3.1415926535897932384626433832795, 1.5707963267948966192313216916398);
             vec3 rayDirection = vec3(cos(thetaphi.y) * cos(thetaphi.x), sin(thetaphi.y), cos(thetaphi.y) * sin(thetaphi.x));
             fragColor = texture(envMap, rayDirection); //vec4(1,0,0,1);
         }
     );
+
+    // Renderer
 
     void Renderer::setup(int size, Scene* s, int internalformat)
     {
@@ -65,8 +66,10 @@ namespace ofxEquiMap {
         int format = ofGetGLFormatFromInternal(internalformat);
         cm.init(size, format, type);
         if (ofIsGLProgrammableRenderer()) {
+            // ofLogWarning() << "using programmable GL renderer";
             warpShader.setupShaderFromSource(GL_VERTEX_SHADER, warp_vert_shader_str330 );
             warpShader.setupShaderFromSource(GL_FRAGMENT_SHADER, warp_frag_shader_str330 );
+            // warpShader.bindDefaults();
         } else {
             // warpShader.setupShaderFromSource(GL_VERTEX_SHADER, warp_vert_shader_str );
             warpShader.setupShaderFromSource(GL_FRAGMENT_SHADER, warp_frag_shader_str);
@@ -92,11 +95,22 @@ namespace ofxEquiMap {
     }
 
     void Renderer::draw(float x, float y, float w, float h) {
-        warpShader.begin();
-        warpShader.setUniform1i("envMap", 0);
-        cm.drawFace(GL_TEXTURE_CUBE_MAP_POSITIVE_Z, x, y, w, h);
-        warpShader.end();
+        if (ofIsGLProgrammableRenderer()) {
+            warpShader.begin();
+            warpShader.setUniform1i("envMap", 0);
+            warpShader.setUniform2f("dimmensions", w, h);
+            cm.bind();
+            ofDrawRectangle(x,y,w,h);
+            warpShader.end();
+        } else {
+            warpShader.begin();
+            warpShader.setUniform1i("envMap", 0);
+            cm.drawFace(GL_TEXTURE_CUBE_MAP_POSITIVE_Z, x, y, w, h);
+            warpShader.end();
+        }
     }
+
+    // CustomFboRenderer
 
     void CustomFboRenderer::setup(int size, Scene* s, int internalformat, int numSamples)
     {
@@ -139,6 +153,8 @@ namespace ofxEquiMap {
         }
     }
 
+    // Exporter
+
     void Exporter::update() {
         if (!fbo.isAllocated()) {
           fbo.allocate(width, height, GL_RGB);
@@ -160,6 +176,8 @@ namespace ofxEquiMap {
         ofSaveImage(pixels, fname, OF_IMAGE_QUALITY_BEST);
         return true;
     }
+
+    // functions
 
     bool saveToFile(Renderer& renderer, const std::string& fileName) {
         auto ex = Exporter(renderer);
